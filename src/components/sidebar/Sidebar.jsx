5222f20e43977f2200996cdd5693ac6cc0a99ce5
5manojrawat5 <5mercy5mercy5@gmail.com>
2024-03-25 12:15:14 +0530

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./sidebar.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/9162803/pexels-photo-9162803.jpeg?auto=compress&cs=tinysrgb&w=260&h=320&dpr=1"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, ipsam. Cupiditate ipsum dolorem voluptate Lorem,
+          ipsum.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+              <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
